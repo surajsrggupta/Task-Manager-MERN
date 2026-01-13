e2f5410e195caeddd59cc01e2801fa1b6aa13af4
surajsrggupta <surajsrggupta@gmail.com>
2026-01-13 11:02:09 +0530

    add loading and ui fixed

--- frontend/src/components/HomeApp.jsx
+++ frontend/src/components/HomeApp.jsx
@@ -2,114 +2,106 @@ import { useEffect, useEffectEvent, useState } from "react";
 import TaskForm from "./TaskForm";
 
 const HomeApp = () => {
+  const [tasks, setTasks] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [tasks, setTasks] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    const effectEventFetchTasks = useEffectEvent(async()=>{
-        try {
-            setLoading(true);
-            const response = await fetch("http://localhost:3000/api/task");
-            const data = await response.json();
-            if(response.ok){
-                setTasks(data);
-                setLoading(false);
-            }
-        } catch (error) {
-            console.log(`Error hua: ${error.message}`);
-            setLoading(false);
-        }
-    })
-
-    useEffect(()=>{
-
-        effectEventFetchTasks();
-
-    },[])
-
-    const handleDelete = async(id)=>{
-        const isConfirmed = window.confirm("Are You Sure?");
-
-        if(!isConfirmed) return;
-
-        const response = await fetch(`http://localhost:3000/api/task/${id}`,{
-            method:"DELETE" ,  
-          });
-
-          if(response.ok){
-            setTasks(tasks.filter((task)=>task._id != id));
-          }
-
+  const effectEventFetchTasks = useEffectEvent(async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:3000/api/task");
+      const data = await response.json();
+      if (response.ok) {
+        setTasks(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(`Error hua: ${error.message}`);
+      setLoading(false);
     }
+  });
 
-    const toggleStatus = async(id, currStatus)=>{
-       
-        const newStatus = currStatus=="pending"? "completed":"pending"; // toggle
-        
-        const response = await fetch(`http://localhost:3000/api/task/${id}`,{
-            method: "PUT",
-            headers:{"Content-Type":"application/json"},
-            body: JSON.stringify({status: newStatus})  
-        })
-         
+  useEffect(() => {
+    effectEventFetchTasks();
+  }, []);
 
-        if(response.ok){
-            setTasks(tasks.map((task)=>(
-                task._id ===id?{...task, status: newStatus}:task
-            )))
-        }
+  const handleDelete = async (id) => {
+    const isConfirmed = window.confirm("Are You Sure?");
+
+    if (!isConfirmed) return;
+
+    const response = await fetch(`http://localhost:3000/api/task/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      setTasks(tasks.filter((task) => task._id != id));
     }
+  };
+
+  const toggleStatus = async (id, currStatus) => {
+    const newStatus = currStatus == "pending" ? "completed" : "pending"; // toggle
+
+    const response = await fetch(`http://localhost:3000/api/task/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: newStatus }),
+    });
+
+    if (response.ok) {
+      setTasks(
+        tasks.map((task) =>
+          task._id === id ? { ...task, status: newStatus } : task,
+        ),
+      );
+    }
+  };
 
   return (
     <div>
-        <h1>MERN Task Manager</h1>
+      <h1>MERN Task Manager</h1>
 
-        {loading?(
-            <div>
-                <div>
-
-                </div>
-                <p>Loading...</p>
-            </div>
-        ):(
-
-  <>
-            <TaskForm/>
-        <div className="box" style={{backgroundColor:"black"}}>
-        {tasks && tasks.map((task)=>(
-            <div className="taskBox" key={task._id} style={{backgroundColor: "green"}}>
-                <input type="checkbox"
-                checked = {task.status==="completed"}
-                onChange={()=>toggleStatus(task._id, task.status)}
-                style={{marginRight: "10px"}}
-                // name="status" id="status"
+      {loading ? (
+        <div className="spinner-container">
+          <div className="spinner"></div>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <>
+          <TaskForm />
+          <div className="box" style={{ backgroundColor: "black" }}>
+            {tasks &&
+              tasks.map((task) => (
+                <div
+                  className="taskBox"
+                  key={task._id}
+                  style={{ backgroundColor: "green" }}
+                >
+                  <input
+                    type="checkbox"
+                    checked={task.status === "completed"}
+                    onChange={() => toggleStatus(task._id, task.status)}
+                    style={{ marginRight: "10px" }}
+                    // name="status" id="status"
                   />
 
-                <h3 style={{textDecoration: task.status=="completed"? "line-through":"none"}}>{task.title}</h3>
-                <p>{task.description}</p>
-                <p>{task.status}</p>
-                <button onClick={()=>handleDelete(task._id)}>Delete</button>
-            </div>
-        ))}
-        </div>
+                  <h3
+                    style={{
+                      textDecoration:
+                        task.status == "completed" ? "line-through" : "none",
+                    }}
+                  >
+                    {task.title}
+                  </h3>
+                  <p>{task.description}</p>
+                  <p>{task.status}</p>
+                  <button onClick={() => handleDelete(task._id)}>Delete</button>
+                </div>
+              ))}
+          </div>
         </>
-      
-
-
-            
-        )
-        
-
-
-
-
-
-
-
-
-    }     
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default HomeApp
+export default HomeApp;
